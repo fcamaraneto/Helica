e1a91255eaeb2ba5088e7c90ee0b592eda63f8c8
Felipe Camara <78320789+fcamaraneto@users.noreply.github.com>
2023-04-26 17:48:13 +0200

    Update CABOTioN.py

--- CABOTioN.py
+++ CABOTioN.py
@@ -2,37 +2,41 @@
 
 import streamlit as st
 from PIL import Image # create page icon
+#from streamlit_extras.app_logo import add_logo
 import streamlit.components.v1 as components
 
 #  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 #                                     SETTINGS
 #  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-icon=Image.open('aau_icon.png')
-st.set_page_config(page_title="CABOTioN", layout="centered", page_icon=icon)
+icon=Image.open('dnv_logo.jpg')
+st.set_page_config(page_title="HELICA Multiphysics", layout="centered", page_icon=icon)
 
 #-  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 #                                     SIDEBAR
 #  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+#add_logo("https://i.ibb.co/z6v0zDn/dnv-logo.jpg")
+
+
 
 def add_logo():
     st.markdown(
         """
         <style>
             [data-testid="stSidebarNav"] {
-                #background-image: url(https://iili.io/gfPxyv.png);
-                #background-image: url(https://iili.io/gKGast.png);
-                background-image: url(https://iili.io/gq211a.png);
+                background-image: url(https://i.postimg.cc/K88r3LRp/dnv-logo.jpg);
                 background-repeat: no-repeat;
-                padding-top: 10px;
-                background-position: 20px 20px;
+                margin-left: 20px;
+                padding-top: 100px;
+                background-position: 1px 1px;
             }
             [data-testid="stSidebarNav"]::before {
-                #content: "My Company Name";
-                margin-left: 20px;
-                margin-top: 20px;
-                font-size: 30px;
-                position: relative;
-                top: 100px;
+              # content: "My Company Name";
+              #  margin-left: 2px;
+              #  margin-top: 2px;
+              #  font-size: 3px;
+              #  position: relative;
+              #  top: 1px;
             }
         </style>
         """,
@@ -40,89 +44,67 @@ def add_logo():
     )
 add_logo()
 
+#st.sidebar.image('dnv_logo.jpg', width=150)
 
-#with st.sidebar.container():
-#    image = Image.open('eu_logo.jpg')
-#st.image(image)
+
+
+st.header("Umbilical, cable and flexible pipe analysis")
+
+
+
+with st.sidebar.container(): 
+    image = Image.open('helica_back.jpg')
+st.image(image)
+
+st.markdown('### Consistent, highly efficient and robust stress and fatigue analysis of umbilicals, cables and flexible risers in subsea applications')
+
+
 
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
 
 # Using object notation
 add_selectbox = st.sidebar.selectbox(
-    "Additional information:",
-    ("Work Packages",
-     "Upcoming HVDC Projects",
-     "Other"))
-
-st.sidebar.image('aau_logo.png', width=150)
-st.sidebar.image('ist_logo.png', width=150)
-st.sidebar.image('energinet_logo.png', width=200)
-st.sidebar.image('orsted_logo.png', width=130)
-st.sidebar.image('pscad_logo2.png', width=180)
+    "Module:",
+    ("Cross-section",
+     "Fatigue",
+     "VIV"))
 # -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-#st.markdown("# CABOTioN Project")
-#st.sidebar.markdown("# CABOTioN ")
-# -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
-#st.header('This is a header')
-#st.title('This is a title')
 
 
-st.header("CABOTioN Project")
 
-#st.markdown('### Summary')
-st.markdown('A sustainable energy network must comply with the usage of offshore wind farms and the sharing of surplus '
-            'renewable energy without a dangerous carbon footprint. Amongst the possible technologies to foster the '
-            'energy transition one way is to rely on HVDC transmission resorting to submarine cables for energy '
-            'transfer. However, field measurements show the lack of confidence in simulation results with regard to '
-            'the voltage and current profiles on the core, sheath or armour. A proper representation of the magnetic '
-            'behaviour of the armour and the impact of the sea and seabed for an overall assessment of the transient '
-            'behaviour remain a challenge.')
+st.markdown('The Helica software module in Sesam includes technology that represents industry consensus for cross section analysis of umbilicals and flexible risers, including fatigue analysis of section components such as helix wires and steel tubes. Using Helica, subsea engineers can reduce the analysis time to a fraction, as thousands of simulations may be performed in minutes. ')
 
-st.markdown('### Project description')
-st.markdown('The EU-funded CABOTioN project fosters the deployment of offshore interconnections and energy islands '
-            'in Europe, by addressing the challenge of modelling high voltage direct current (HVDC) submarine cables. '
-            'As HVDC submarine cables are quite complex, the project proposes the design of an innovative '
-            'special-purpose tool to compute cable parameters, to allow a straightforward and systematic '
-            'implementation as a reliable digital twin. This will increase the confidence of evaluations, '
-            'by avoiding simplifications due to the lack of accurate models for the assessment of transient and '
-            'steady-state behaviour involving offshore energy transmission. CABOTioN will contribute to the '
-            'development of cost-effective green projects by helping to find new and more economical ways of '
-            'building HVDC offshore transmission networks.')
+st.markdown('### Cross-section and fatigue analysis')
+st.markdown('Helica is tailor-made software for cross-section analysis of flexible pipes and umbilicals and is validated against high quality stress measurements in a full-scale subsea umbilical.')
 
-st.markdown('### Objectives')
-st.markdown('The overall objective of CABOTioN is the development of an innovative approach for identification of '
-            'per-unit-length parameters of submarine DC cables and a straightforward interface with simulation tools. '
-            'The applicant will resort to up-to-date formulations to design a special-purpose tool to compute '
-            'electrical parameters considerably faster than the Finite Element Method without a significant '
-            'loss of accuracy. Therefore, evaluations will be conducted to assess the enhancements achieved with '
-            'a more accurate model and compared with the common practice and measurements.')
+
+st.markdown('### Stresses in dynamic umbilicals and flexible pipes')
+st.markdown('Accurate evaluation of local stresses in complex cross-sections, e.g. of subsea umbilicals and flexible pipes, '
+            'subjected to extreme and fatigue loading, is a challenge to the industry. Fatigue of cross-section components such '
+            'as helix tensile armors and steel tubes is a critical design issue for dynamic umbilicals and flexible pipes.')
+st.markdown('The basis for assessment of fatigue damage of such elements is the long-term stress cycle distribution at critical locations on the helix '
+            'elements caused by long-term environmental loading on the system. The long-term stress cycle distribution will therefore require '
+            'global dynamic time domain analysis followed by a detailed cross-section analysis in a large number of irregular sea states.')
+st.markdown('Helica is a vital component in an overall computational consistent and highly efficient fatigue analysis scheme.')
+
+
+#st.markdown('## Main functionalities:')
+st.markdown('### Key features')
 st.markdown("""
-    - Develop a computational tool for parameter computation of power cables
-    - Investigate special cable design configurations not addressed
-    - Verification of ...
+    - Cross-sectional load sharing analysis
+    - Short-term fatigue analysis
+    - Long-term fatigue analysis
+    - Extreme analysis
+    - Load-sharing between elements
+    - Calculation of cross-sectional stiffness properties
+    - Evaluation of stick/slip behaviour due to interlayer frictional forces
+    - Stresses in selected components due to combined bending and tensile loading
+    - Assessment of capacity curves for combined bending and tensile loading of the cross section
+    - Validation against high quality stress measurements in full-scale tests of umbilicals
+    - Compatible with result files (time series) generated by global analysis software such as Orcaflex, Riflex and Flexcom
     """)
 
 
 
-st.markdown("""
-    ### Supervision
-    - Prof. Filipe Faria da Silva (Aalborg University)
-    - Prof. Claus Leth Bak (Aalborg University)         
-    - Profa. Maria Teresa Correia de Barros (Instituto Superior Técnico / Lisbon University) 
-    - Prof. Antonio Carlos Siqueira de Lima (Federal University of Rio de Janeiro)
-    """)
 
-st.markdown("""
-    ### Grant
-    This project has received funding from the European Union’s Horizon 2020 research and innovation programme 
-    under the Marie Skłodowska-Curie grant agreement No 101031088 ([CORDIS](https://cordis.europa.eu/project/id/101031088))”.     
-    """)
-
-st.markdown('<a href="/02_MoM-SO.py" target="_self">Next page</a>', unsafe_allow_html=True)
-
-
-# - Marie-Curie Postdoctoral Fellowship
-#     - HORIZON 2020: EU-funded research project
-#     - Project description at [CORDIS database](https://cordis.europa.eu/project/id/101031088)
-#     - Grant agreement: 101031088
-#     - Duration: September 2021 - August 2023
+Project description at [CORDIS database](https://www.dnv.com/services/umbilical-analysis-and-flexible-pipe-analysis-helica-69553)
