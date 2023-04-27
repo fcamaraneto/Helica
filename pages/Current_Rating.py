0ec9c90c3decdbb4fb1b7d1658605bf018bef14b
Felipe Camara <78320789+fcamaraneto@users.noreply.github.com>
2023-04-27 16:46:48 +0200

    Update Current_Rating.py

--- pages/Current_Rating.py
+++ pages/Current_Rating.py
@@ -79,33 +79,54 @@ with tab1:
         #    st.image(image, caption='Cross Section', width=250)
 
         fig = go.Figure()
-        fig.update_xaxes(range=[-5, 5], zeroline=False)
-        fig.update_yaxes(range=[-5, 5])
+
+        ra = 0.2
+        outer = 4
+        n = 60
+        theta = 360 / n
+
+        x = [outer * np.cos(i * (theta * np.pi / 180)) for i in range(0, n)]
+        y = [outer * np.sin(i * (theta * np.pi / 180)) for i in range(0, n)]
+
+        # Add circles
+
+        for i in range(n):
+            fig.add_shape(type="circle",
+                          x0=x[i] - ra, y0=y[i] - ra, x1=x[i] + ra, y1=y[i] + ra,
+                          line_color="LightSeaGreen")
+
+        fig.add_shape(type="circle",
+                      x0=-outer - 2 * ra, y0=-outer - 2 * ra, x1=outer + 2 * ra, y1=outer + 2 * ra,
+                      line_color="LightSeaGreen");
+
+        fig.add_shape(type="circle",
+                      x0=-outer - 1 * ra, y0=-outer - 1 * ra, x1=outer + 1 * ra, y1=outer + 1 * ra,
+                      line_color="LightSeaGreen");
+
+        fig.add_shape(type="circle",
+                      x0=-outer + 1 * ra, y0=-outer + 1 * ra, x1=outer - 1 * ra, y1=outer - 1 * ra,
+                      line_color="LightSeaGreen");
 
         fig.add_shape(type="circle",
                       x0=-3, y0=-3, x1=3, y1=3,
-                      line_color="LightSeaGreen")
+                      line_color="LightSeaGreen");
 
         fig.add_shape(type="circle",
                       x0=-3.2, y0=-3.2, x1=3.2, y1=3.2,
                       line_color="LightSeaGreen")
 
         fig.add_shape(type="circle",
-                      x0=-4.2, y0=-4.2, x1=4.2, y1=4.2,
-                      line_color="LightSeaGreen")
-
-        fig.add_shape(type="circle",
-                      x0=-4., y0=-4., x1=4., y1=4.,
-                      line_color="LightSeaGreen")
-
-        fig.add_shape(type="circle",
                       xref="x", yref="y",
                       fillcolor="PaleTurquoise",
-                      x0=-1, y0=-1, x1=1, y1=1,
-                      line_color="LightSeaGreen")
+                      x0=-1.5, y0=-1.5, x1=1.5, y1=1.5,
+                      line_color="LightSeaGreen");
 
-        # Set figure size
         fig.update_layout(width=600, height=600)
+        fig.update_xaxes(range=[-5, 5], zeroline=False)
+        fig.update_yaxes(range=[-5, 5])
+
+        fig.update_xaxes(visible= False,mirror=True, ticks='outside', showline=True, linecolor='black', gridcolor='white')
+        fig.update_yaxes(visible= False,mirror=True, ticks='outside', showline=True, linecolor='black', gridcolor='white')
 
         st.plotly_chart(fig)
 
