6c3323538ab564964eb1cfb04c9d682ca954b067
Felipe Camara <78320789+fcamaraneto@users.noreply.github.com>
2023-05-02 15:15:24 +0200

    Update and rename Current_Rating_ok.py to Current_Rating_test.py

---
+++ pages/Current_Rating_test.py
@@ -0,0 +1,171 @@
+import streamlit as st
+from PIL import Image # create page icon
+
+import pandas as pd
+import numpy as np
+#import scipy.io as spio
+import scipy.special as spios
+import plotly.express as px
+
+import plotly.graph_objects as go
+
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#                                     SETTINGS
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+icon=Image.open('dnv_logo.jpg')
+st.set_page_config(page_title="HELICA Multiphysics", layout="centered", page_icon=icon)
+
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#                                     SIDEBAR
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+def add_logo():
+    st.markdown(
+        """
+        <style>
+            [data-testid="stSidebarNav"] {
+                background-image: url(https://i.postimg.cc/K88r3LRp/dnv-logo.jpg);
+                background-repeat: no-repeat;
+                margin-left: 20px;
+                padding-top: 100px;
+                background-position: 1px 1px;
+            }
+            [data-testid="stSidebarNav"]::before {
+              # content: "My Company Name";
+              #  margin-left: 2px;
+              #  margin-top: 2px;
+              #  font-size: 3px;
+              #  position: relative;
+              #  top: 1px;
+            }
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+url='https://i.postimg.cc/NjhVmdYR/helica-logo.png'
+
+st.markdown(
+        f"""
+        <style>
+            [data-testid="stSidebarNav"] + div {{
+                position:relative;
+                bottom: 0;
+                height:65%;
+                background-image: url({url});
+                background-size: 40% auto;
+                background-repeat: no-repeat;
+                background-position-x: center;
+                background-position-y: bottom;
+            }}
+        </style>
+        """,
+        unsafe_allow_html=True,
+    )
+
+# '📊 📉 📈 📧 🗂️ 📂 📈  🖥️🗄️  '
+
+add_logo()
+#st.sidebar.image('aau_logo.png', width=150)
+
+st.sidebar.markdown("HELICA Cable Rating module complies with IEC 60287 and IEC ... ")
+
+
+
+
+
+
+
+
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#                                     CROSS-SECTION
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+st.title("HELICA Current Rating")
+st.markdown('The Cable Rating module ... ')
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+tab1, tab2, tab3 = st.tabs(["🖥️ Cable Data", "📊 Cable Rating", "🗂️ Export Results"])
+
+with tab1:
+    cable2 = st.selectbox("Select Cable Type",
+                       options=["Single Core", "Three Core"])
+
+
+
+
+
+
+
+    col1, col2, col3 = st.columns([1, 8, 1])
+    with col1:
+        ''
+    with col2:
+        ''
+
+
+
+
+
+
+
+
+
+    with col3:
+        ''
+
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#                                CURRENT RATING - RESULTS
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+
+with tab2:
+
+    st.markdown(' ')
+    st.markdown(' ')
+    st.markdown(' ')    
+
+
+
+
+
+
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#                                CURRENT RATING - OUTPUT
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+#  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -  -
+
+with tab3:
+    #st.subheader('Interfacing with circuit solvers')
+    #st.markdown(' Interfacing with circuit solvers contains matlab scripts which demonstrate'
+    #        ' how to interface rational function-based models with time domain circuit solvers '
+    #        'via a Norton equivalent. The procedure is shown for models representing '
+    #        'Y-parameters, Z-parameters, S-parameters, and general transfer functions that '
+    #        'do not interact with the circuit.')
+
+    #col = st.selectbox("Select Software:",
+    #                   options=["PSCAD", "EMTP", "PowerFactory", "ATP"])
+
+    import time
+    localtime = time.asctime(time.localtime(time.time()))
+    dum = time.strftime("Date:%d-%m-%Y  Time:%H:%M:%S", time.localtime())
+
+
+    st.write("")
+
+    st.download_button(
+        label="Download Data",
+        data='Universal Cable Constants (UCC) \n\n' + dum,
+        file_name='cable_parameters.csv',
+        mime='text/csv')
+
+    st.download_button(
+        label="Download Report",
+        data='Universal Cable Constants (UCC) \n\n' + dum,
+        file_name='cable_parameters.txt',
+        mime='text/csv')
+
+
+
+
